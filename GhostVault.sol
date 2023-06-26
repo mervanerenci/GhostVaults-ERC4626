d0f60030e6c0578d24cd48f3ae558581908d0404
Mervan <mervanerenci@gmail.com>
2023-06-26 22:54:06 +0300

    deposit fixes

--- GhostVault.sol
+++ GhostVault.sol
@@ -21,49 +21,34 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract GhostVault is ERC4626{
+contract GhostVault is ERC4626 {
 
     // address constant USDC_ADDRESS = 0xe9DcE89B076BA6107Bb64EF30678efec11939234;
     address private constant POOL_ADDRESS_PROVIDER = 0xeb7A892BB04A8f836bDEeBbf60897A7Af1Bf5d7F;
     address private AAVE_LENDING_POOL_ADDRESS; 
-    address private constant PRICE_ORACLE;
+    address private  PRICE_ORACLE;
     
-    // mapping for users to track their deposited balances
+    
     mapping(address => uint256) public balances;
     
-    // IERC20 public USDC = IERC20(USDC_ADDRESS);
+  
     IPool private lendingPool;
     IPriceOracle private priceOracle;
 
-    ERC20 public immutable asset;
-
-    /*//////////////////////////////////////////////////////////////
-                                 EVENTS
-    //////////////////////////////////////////////////////////////*/
-
-    event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares);
-
-    event Withdraw(
-        address indexed caller,
-        address indexed receiver,
-        address indexed owner,
-        uint256 assets,
-        uint256 shares
-    );
+    
 
     /*//////////////////////////////////////////////////////////////
                                IMMUTABLE
     //////////////////////////////////////////////////////////////*/
 
-    ERC20 public immutable asset;
+    
 
     constructor(
-        ERC20 _asset,
+        ERC20 _token,
         string memory _name,
         string memory _symbol
-    ) ERC20(_name, _symbol, _asset.decimals()) {
-        asset = _asset;
-
+    ) ERC4626(_token, _name, _symbol)  {
+        
         IPoolAddressesProvider provider = IPoolAddressesProvider(POOL_ADDRESS_PROVIDER);
         AAVE_LENDING_POOL_ADDRESS = provider.getPool();
         lendingPool = IPool(AAVE_LENDING_POOL_ADDRESS);
@@ -79,7 +64,7 @@ contract GhostVault is ERC4626{
         require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
 
         // Need to transfer before minting or ERC777s could reenter.
-        asset.safeTransferFrom(msg.sender, address(this), assets);
+        asset.transferFrom(msg.sender, address(this), assets);
 
         _mint(receiver, shares);
 
@@ -88,69 +73,70 @@ contract GhostVault is ERC4626{
         afterDeposit(assets, shares);
     }
 
-    function afterDeposit(uint256 assets, uint256 /*shares*/) internal virtual {
-        // balances[msg.sender] += assets;
-        // Get  balance
-        uint256 userBalance = asset.balanceOf(msg.sender);
-        // Ensure the user has enough balance to deposit
-        require(userBalance >= amount, "Insufficient USDC balance"); 
-        // Transfer  tokens to this smart contract
-        require(asset.transferFrom(msg.sender, address(this), amount), "USDC transfer failed");
-
+    function afterDeposit(uint256 assets, uint256 /*shares*/) internal virtual override {
+    
         // Approve lending pool to use  tokens from this smart contract
-        require(asset.approve(AAVE_LENDING_POOL_ADDRESS, amount), "Approval failed");
+        asset.approve(AAVE_LENDING_POOL_ADDRESS, assets);
 
         // Deposit  tokens to the Aave lending pool
-        lendingPool.supply(asset, amount, address(this), 0);
+        lendingPool.supply(address(asset), assets, address(this), 0);
     }
 
     /*//////////////////////////////////////////////////////////////
                             ACCOUNTING LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function totalAssets() public view virtual returns (uint256);
+    function totalAssets() public view virtual override returns (uint256) {
+        // aTokens use rebasing to accrue interest, so the total assets is just the aToken balance
+        return asset.balanceOf(address(this));
+    }
+    
 
-    function convertToShares(uint256 assets) public view virtual returns (uint256) {
-        return  assets;
+    function convertToShares(uint256 assets) public view virtual override returns (uint256) {
+        uint256 supply = assets;
+        return  supply;
     }
 
-    function convertToAssets(uint256 shares) public view virtual returns (uint256) {
-        return  assets;
+    function convertToAssets(uint256 shares) public view virtual override returns (uint256) {
+        uint256 supply = shares;
+        return  supply;
     }
 
-    function previewDeposit(uint256 assets) public view virtual returns (uint256) {
+    function previewDeposit(uint256 assets) public view virtual override returns (uint256) {
         return convertToShares(assets);
     }
 
-    function previewMint(uint256 shares) public view virtual returns (uint256) {
-        return  assets;
+    function previewMint(uint256 shares) public view virtual override returns (uint256) {
+        uint256 supply = shares;
+        return  supply;
     }
 
-    function previewWithdraw(uint256 assets) public view virtual returns (uint256) {
-        return  assets;
+    function previewWithdraw(uint256 assets) public view virtual override returns (uint256) {
+        uint256 supply = assets;
+        return  supply;
     }
 
-    function previewRedeem(uint256 shares) public view virtual returns (uint256) {
+    function previewRedeem(uint256 shares) public view virtual override returns (uint256) {
         return convertToAssets(shares);
     }
 
 
 
 
-    // INTERNAL HOOKS //
+   
 
-    function getPriceOracleAddress() public view returns (address) {
+    // function getPriceOracleAddress() public view returns (address) {
 
-        PRICE_ORACLE = lendingPool.getPriceOracle();   
-        return PRICE_ORACLE;
+    //     PRICE_ORACLE = lendingPool.getPriceOracle();   
+    //     return PRICE_ORACLE;
 
-    }
+    // }
 
-    function getAssetPrice( ) public view  returns (uint256) {
+    // function getAssetPrice( ) public view  returns (uint256) {
 
-        return priceOracle.getAssetPrice(USDC_ADDRESS);
+    //     return priceOracle.getAssetPrice(USDC_ADDRESS);
 
-    }
+    // }
 
     
 
